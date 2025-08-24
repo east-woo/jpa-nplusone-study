6affe364e1b88adfdf78c16d77a0a3b2092f6acf
dongwoo <dwseo96@naver.com>
2025-08-25 00:47:15 +0900

    Initial commit

---
+++ src/main/java/com/eastwoo/study/jpanplusonestudy/JpaNplusoneStudyApplication.java
@@ -0,0 +1,13 @@
+package com.eastwoo.study.jpanplusonestudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaNplusoneStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpaNplusoneStudyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/eastwoo/study/jpanplusonestudy/JpaNplusoneStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eastwoo.study.jpanplusonestudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaNplusoneStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
